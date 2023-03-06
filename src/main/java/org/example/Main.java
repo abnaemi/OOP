944cd158d4c09a3dce16c8e7f0ccfbde9c812e52
abnae <ab.naemi@gmail.com>
2023-03-06 11:11:41 +0100

    Created student class. Added few variables.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args){
 
+    student firstStudent = new student();
 
+    firstStudent.setFirstname("Max");
+    firstStudent.setSurname("Mustermann");
+    firstStudent.setStudentID(2022297);
 
-
+    System.out.println(firstStudent.getfirstName());
+    System.out.println(firstStudent.getSurname());
+    System.out.println(firstStudent.getStudentID());
 
 
     }
--- src/main/java/org/example/student.java
+++ src/main/java/org/example/student.java
@@ -2,8 +2,41 @@ package org.example;
 
 public class student {
 
-    int studentID;
-    String firstname;
-    String surname;
+    private int studentID;
+    private String firstname;
+    private String surname;
+
+    public int getStudentID(){
+        return studentID;
+
+    }
+
+    public void setStudentID (int x){
+        studentID = x;
+
+    }
+
+
+    public String getfirstName(){
+        return firstname;
+
+    }
+
+    public void setFirstname (String newFirstname){
+        firstname = newFirstname;
+
+    }
+
+    public String getSurname(){
+        return surname;
+
+    }
+
+    public void setSurname (String newSurname){
+        surname = newSurname;
+
+    }
+
+
 
 }
